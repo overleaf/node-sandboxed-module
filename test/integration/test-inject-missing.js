70c87ba26d1ebb22d38fc1caa6eb0c8dd1e821e0
Gareth Jones <gareth.jones@shinetech.com>
2018-01-25 08:41:56 +1100

    handle missing modules in submodules required from sandboxed modules

--- test/integration/test-inject-missing.js
+++ test/integration/test-inject-missing.js
@@ -1,9 +1,14 @@
 var assert = require('assert');
 var SandboxedModule = require('../..');
 
-var foo = SandboxedModule.require('../fixture/foo', {
-	"requires": {"doesNotExist": {}},
+var foo = SandboxedModule.require('../fixture/loads-missing', {
+	"requires": {
+    "doesNotExist": { fake: true },
+    "./alsoDoesNotExist": { cheese: 'gouda' },
+    "requiredFromSubModule": { didItWork: 'yes' }
+  },
 	"ignoreMissing": true
 });
-assert.strictEqual(foo.foo, 'foo');
-assert.strictEqual(foo.bar, 'bar');
+assert.ok(foo.doesNotExist.fake);
+assert.strictEqual(foo.alsoDoesNotExist.cheese, 'gouda');
+assert.strictEqual(foo.subModule.requiredFromSubModule.didItWork, 'yes');
