6d9301d0ef8198ebe9cb1b44f42a9243e125dfde
Gareth Jones <gareth.jones@shinetech.com>
2019-05-03 08:49:40 +1000

    fixes to get the tests passing in node 12

--- scripts/generate_builtin_modules_list.js
+++ scripts/generate_builtin_modules_list.js
@@ -3,7 +3,8 @@ var fs = require('fs');
 var path = require('path');
 
 var modules = Object.keys(process.binding('natives')).filter(function (module) {
-    return module.indexOf('internal/') !== 0;
+  // 'config' appears in the list of natives, but you can't 'require' it
+    return module.indexOf('internal/') !== 0 && module.indexOf('config') < 0;
 });
 
 fs.writeFileSync(path.resolve(__dirname, '../lib/builtin_modules.json'), JSON.stringify(modules, undefined, 2));
--- test/integration/test-istanbul.js
+++ test/integration/test-istanbul.js
@@ -5,7 +5,7 @@ SandboxedModule.registerBuiltInSourceTransformer('istanbul');
 function testIt(coverageVariable) {
     global[coverageVariable] = {};
     var baz = SandboxedModule.load('../fixture/baz').exports,
-        instrumentedFunction = /^function \(\){__cov_.*\.f\['1'\]\+\+;__cov_.*\.s\['2'\]\+\+;return 1\+3;}$/;
+        instrumentedFunction = /^function\s?\(\){__cov_.*\.f\['1'\]\+\+;__cov_.*\.s\['2'\]\+\+;return 1\+3;}$/;
 
     assert.strictEqual(baz.biz.toString().match(instrumentedFunction).length, 1);
 
