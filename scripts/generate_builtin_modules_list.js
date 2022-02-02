52e879ae2368ff8ed0a42d100d41a229aa0c06ce
Jakob Ackermann <jakob.ackermann@overleaf.com>
2022-02-02 13:09:57 +0000

    [misc] fix tests for NodeJS version 14 on Linux

--- scripts/generate_builtin_modules_list.js
+++ scripts/generate_builtin_modules_list.js
@@ -3,8 +3,20 @@ var fs = require('fs');
 var path = require('path');
 
 var modules = Object.keys(process.binding('natives')).filter(function (module) {
-  // 'config' appears in the list of natives, but you can't 'require' it
-    return module.indexOf('internal/') !== 0 && module.indexOf('config') < 0;
+    if (module.includes('wasi')) {
+        // web assembly interface
+        return false
+    }
+    if (module.includes('config')) {
+        // 'config' appears in the list of natives, but you can't 'require' it
+        return false
+    }
+    if (module.includes('internal/')) {
+        // ignore internal modules
+        return false
+    }
+    // likely importable
+    return true
 });
 
 fs.writeFileSync(path.resolve(__dirname, '../lib/builtin_modules.json'), JSON.stringify(modules, undefined, 2));
